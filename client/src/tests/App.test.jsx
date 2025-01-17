38af125b6d9e3ee76aab11df63cc841258796d50
toldpixel <software.nutshell@gmail.com>
2025-01-17 16:04:34 +0100

    fix: bug fix

---
+++ client/src/tests/App.test.jsx
@@ -0,0 +1,22 @@
+import { describe, it, expect, beforeEach, vi } from "vitest";
+import App from "../App";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { mockData, mockTitles } from "./mock_data";
+
+beforeEach(() => {
+  global.fetch = vi.fn(() => {
+    return Promise.resolve({
+      ok: true,
+      status: 200,
+      json: () => Promise.resolve(mockData),
+    });
+  });
+});
+
+describe("App", () => {
+  it("fetches and displays all titles", async () => {
+    render(<App />);
+    screen.debug();
+    
+  });
+})
