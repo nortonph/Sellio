ed0b5e5524c4e4f39e6a5daf6e13a799b8389d89
p <philipp.norton@posteo.de>
2025-01-17 17:48:10 +0100

    simplify dbconnect() by getting env variables inside the function

--- server/config/dbconnect.js
+++ server/config/dbconnect.js
@@ -1,17 +1,20 @@
+const path = require('node:path');
+
 require('dotenv').config({
-  path:
+  path: path.join(__dirname,
     process.env.NODE_ENV === 'test'
-      ? '../.env.test.local'
-      : '../.env.development.local',
+      ? '.env.test.local'
+      : '.env.development.local',
+  )
 });
 
-function dbconnect(
-  DB_PROT,
-  DB_SRV,
-  DB_HOST,
-  DB_PORT,
-  DB_NAME
-) {
+const DB_PROT = process.env.DB_PROT;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_NAME = process.env.DB_NAME;
+const DB_SRV = process.env.DB_SRV;
+
+function dbconnect() {
   if (DB_SRV !== '') {
     return `${DB_PROT}://${DB_SRV}@${DB_PORT}/${DB_NAME}`;
   } else {
