d71b11cde822ac11b7f0337829257269facbc613
toldpixel <software.nutshell@gmail.com>
2025-01-17 13:11:17 +0100

    change: remove comment

--- server/config/dbconnect.js
+++ server/config/dbconnect.js
@@ -7,7 +7,6 @@ function dbconnect(
   DB_PORT = "27017",
   DB_NAME = "sellio"
 ) {
-  console.log("test:",DB_SRV);
   if (DB_SRV !== "") {
     return `${DB_PROT}://${DB_SRV}@${DB_PORT}/${DB_NAME}`;
   } else {
