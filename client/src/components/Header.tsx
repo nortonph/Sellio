3fddec70eeab41b782cc5d553502620a76c5c856
Andreas Reinelt <andreasreinelt@me.com>
2025-01-17 12:57:37 +0100

    FIXED BUG: Search does work, but not clicking enter -> added onKeyDown to Header input box

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -75,6 +75,7 @@ function Header(){
                   className="flex-grow bg-transparent outline-none text-gray-700 placeholder-gray-500"
                   value={searchQuery}
                   onChange={(e) => setSearchQuery(e.target.value)}
+                  onKeyDown={(e) => { if (e.key === 'Enter') { handleSearch(); }}}
                 />
                 <button className="text-gray-500 hover:text-gray-700 focus:outline-none" onClick={handleSearch}>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
