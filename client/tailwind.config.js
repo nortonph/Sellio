cc585d7024f9893b6beb972dfd437fe5762303e3
toldpixel <software.nutshell@gmail.com>
2025-01-23 11:57:09 +0100

    add: chat functionality

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: 'jit',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        spin: 'spin 2s linear infinite',
+      },
+      backgroundImage: {
+        'conic-gradient': 'conic-gradient(from 90deg at 50% 50%, #E2CBFF 0%, #393BB2 50%, #E2CBFF 100%)',
+      },
+    },
   },
   plugins: [],
 }
